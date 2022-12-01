6d1184b632328b347e8afa76759ce16ec3477a76
Kunlapath <kunlapath.paengsa@gmail.com>
2022-12-01 13:54:59 +0700

    Add Unit of work

---
+++ MaxCodeFirst.DALs/Repositories/Commons/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace MaxCodeFirst.DALs.Repositories.Commons
+{
+    public interface IUnitOfWork
+    {
+
+        IRepository<T> GetRepository<T>() where T : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ MaxCodeFirst.DALs/Repositories/Commons/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxCodeFirst.DALs.Repositories.Commons
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbContext _dbContext;
+
+        public UnitOfWork(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public IRepository<T> GetRepository<T>() where T : class
+        {
+            return new Repository<T>(_dbContext);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
