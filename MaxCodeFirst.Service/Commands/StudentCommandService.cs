f142d2c671fa72a2eb8a75abdce85186e8c6d917
Kunlapath <kunlapath.paengsa@gmail.com>
2022-12-01 14:01:07 +0700

    Create AddStudent Service

--- MaxCodeFirst.Service/Commands/StudentCommandService.cs
+++ MaxCodeFirst.Service/Commands/StudentCommandService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MaxCodeFirst.DALs.Repositories.Commands;
+using MaxCodeFirst.DALs.Repositories.Commons;
+using MaxCodeFirst.Models.TablesAndEntities;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,24 @@ namespace MaxCodeFirst.Service.Commands
 {
     public class StudentCommandService : IStudentCommandService
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IStudentCommandRepository _repository;
+
+        public StudentCommandService(IUnitOfWork unitOfWork, IStudentCommandRepository repository)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+        }
+
+        public async Task<int> AddStudentAsync(Student student)
+        {
+            var newStudent = new Student
+            {
+                Name = "Max",
+                Description = "test"
+            };
+            await _repository.AddAsync(newStudent);
+            return await _unitOfWork.SaveChangesAsync();
+        }
     }
 }
